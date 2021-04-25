48de6d754278af4ea763deceab4a5cb59ba33b30
asozanska <ada.sozanska@gmail.com>
2021-04-25 15:09:09 +0200

    Rename player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class PlayerMovement : MonoBehaviour {
+public class Player : MonoBehaviour {
     public Rigidbody2D rigidBody;
     public SpriteRenderer renderer;
     public LayerMask groundLayer;
