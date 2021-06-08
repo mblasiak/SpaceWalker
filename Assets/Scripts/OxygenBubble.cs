9a52890217814bca1a0eb23fdedd6cd2963dd864
Miłosz <milblasiak@gmail.com>
2021-06-09 00:27:24 +0200

    Add bubble & plant sharers & bubble decreasing

--- Assets/Scripts/OxygenBubble.cs
+++ Assets/Scripts/OxygenBubble.cs
@@ -2,35 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OxygenBubble : MonoBehaviour {
-
+public class OxygenBubble : MonoBehaviour
+{
     public OxygenBar oxygenBar;
     public float drainSpeed;
-    
+
     private SpriteRenderer renderer;
     private bool playerInside = false;
 
-    void Start() {
+    private Collider2D colider;
+
+    void Start()
+    {
         renderer = this.GetComponent<SpriteRenderer>();
+        colider = this.GetComponent<Collider2D>();
     }
 
-    private void OnTriggerEnter2D(Collider2D collider) {
-        if (collider.CompareTag("Player")) {
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
             playerInside = true;
         }
     }
-    
-    private void OnTriggerExit2D(Collider2D collider) {
-        if (collider.CompareTag("Player")) {
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
             playerInside = false;
         }
     }
-    void Update() {
-        if (playerInside && renderer.color.a > 0) {
+
+    void Update()
+    {
+        if (playerInside && renderer.color.a > 0)
+        {
             Color color = renderer.color;
             color.a -= Time.deltaTime * drainSpeed;
             renderer.color = color;
             oxygenBar.IncreaseOxygenLevel();
+            var currentLengthToBottom = renderer.bounds.extents.y;
+            transform.localScale = transform.localScale - Vector3.one * (Time.deltaTime * drainSpeed * 1f);
+            var verticalOffset = currentLengthToBottom - renderer.bounds.extents.y;
+            transform.position = new Vector3(transform.position.x, transform.position.y - verticalOffset, transform.position.z);
         }
     }
 }
